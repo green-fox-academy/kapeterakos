4e594dfd0ed204186b1a73f0c5d9178f703c7f9d
kapeterakos <kapeter.akos@gmail.com>
2018-10-30 13:53:40 +0100

    todo print

---
+++ Week-02/Day-02/todo_print/main.cpp
@@ -0,0 +1,45 @@
+
+#include <iostream>
+
+#include <string>
+
+
+
+int main(int argc, char* args[])
+
+{
+
+    std::string todoText = " - Buy milk\n";
+
+    // Add "My todo:" to the beginning of the todoText
+
+    // Add " - Download games" to the end of the todoText
+
+    // Add " - Diablo" to the end of the todoText but with indention
+
+
+
+    // Expected outpt:
+
+
+
+    // My todo:
+
+    //  - Buy milk
+
+    //  - Download games
+
+    //      - Diablo
+
+    std::string nextline = " - Download game\n \t - Diablo";
+
+
+    todoText.append(nextline);
+
+    std::cout << todoText << std::endl;
+
+
+
+    return 0;
+
+}
