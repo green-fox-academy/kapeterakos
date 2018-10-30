f2672aa11f7971756ed45216625fb8662e54028c
kapeterakos <kapeter.akos@gmail.com>
2018-10-30 12:19:56 +0100

    sd

---
+++ Week-02/Day-02/simple_replace/main.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+
+#include <string>
+
+
+
+int main(int argc, char* args[])
+
+{
+
+    std::string example("In a dishwasher far far away");
+    std::string from = "dishwasher";
+    int start_pos = example.find(from);
+    if(start_pos == std::string::npos)
+        std::cout << "Cannot replace" << std::endl;
+    else
+        example.replace(start_pos, from.length(), "galaxy");
+
+
+    // I would like to replace "dishwasher" with "galaxy" in this example, but it has a problem.
+
+    // Please fix it for me!
+
+    // Expected output: In a galaxy far far away
+
+
+
+    std::cout << example << std::endl;
+
+    return 0;
+
+}
