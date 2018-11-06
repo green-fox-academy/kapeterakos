10687ba23606af1c693d9236ceb7a1db7b3b7686
kapeterakos <kapeter.akos@gmail.com>
2018-11-06 13:59:30 +0100

    print array with pointer

--- Week-03/Day-02/print_address/main.cpp
+++ Week-03/Day-02/print_address/main.cpp
@@ -8,12 +8,12 @@ int main()
     int input[5] = {};
 
     std::cout<<"Give me five numbers"<<std::endl;
-    for (int  i = 0; i < sizeof(input) / sizeof(input[0]); ++i) {
-        std::cin>> i;
+    for (int i = 0; i < sizeof(input)/ sizeof(input[0]); ++i) {
+        std::cin>>input[i];
 
     }
     int *inputpointer= input;
-    for (int j = 0; j < sizeof(input) / sizeof(input[0]); ++j) {
+    for (int j = 0; j < sizeof(input) / sizeof(input[0]) ; ++j) {
         std::cout<<inputpointer + j <<std::endl;
     }
 
