0508771173a53879c954acd1f3fb11746d55a609
kapeterakos <kapeter.akos@gmail.com>
2018-11-15 11:14:05 +0100

    counter(recursive)

--- Week-04/Day-04/counter/main.cpp
+++ Week-04/Day-04/counter/main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
+
+
+int counter(int n)
+{
+    if(n ==0)
+        return 0;
+    std::cout<<n<<std::endl;
+    counter(n-1);
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int num = 5;
+    std::cout<<counter(num);
     return 0;
 }
