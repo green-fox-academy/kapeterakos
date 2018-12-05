db68c2a72bfbecedc3802460ccf92a82ea0f67e6
kapeterakos <kapeter.akos@gmail.com>
2018-12-05 12:17:40 +0100

    Hello me exercise

---
+++ Week-06/Day-03/hellome/main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+
+{
+    // Modify this program to greet you instead of the World!
+    char *name;
+    printf("Who are you?\n");
+    scanf("%s",&name);
+    printf("Hello %s",&name);
+
+
+    return 0;
+
+}
