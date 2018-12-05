5dd1369dd3e845e09a30e5c23a3d6eba2a0f82f7
kapeterakos <kapeter.akos@gmail.com>
2018-12-05 13:08:38 +0100

    Introduce yourself C

---
+++ Week-06/Day-03/introduce_yourself_c/main.cpp
@@ -0,0 +1,33 @@
+
+#include<stdio.h>
+
+
+
+int main()
+
+{
+    //  Example output:
+
+    //  John Doe
+
+    //  31
+
+    //  1.87
+
+    char name[20];
+    int age;
+    float height;
+
+
+
+    printf("Please give me your name\n");
+    scanf("%s",&name);
+    printf("Please give me your age\n");
+    scanf("%d",&age);
+    printf("Please give me your height in meters\n");
+    scanf("%f",&height);
+
+    printf("%s " "%d " "%f \n",name,age,height);
+    return 0;
+
+}
