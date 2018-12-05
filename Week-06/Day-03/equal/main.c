262c18d552a2655b88265539f4a23188b4ae9a80
kapeterakos <kapeter.akos@gmail.com>
2018-12-05 16:46:27 +0100

    equal in C

--- Week-06/Day-03/equal/main.c
+++ Week-06/Day-03/equal/main.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void equal(int num1, int num2);
+int equal(int num1, int num2);
 int main()
 {
     int x = 0, y = 0;
-    equal(x,y);
+   printf("%d", equal(x,y));
     return 0;
 }
 
-void equal(int num1, int num2){
+int equal(int num1, int num2){
     printf("Give a number");
     scanf("%d",&num1);
     printf("Give another number");
     scanf("%d",&num2);
     if(num1 == num2){
-       printf("1");
+        return 1;
     } else
-        printf("0");
+        return 0;
 }
